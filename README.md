fac42ee44c5612770ae78c69beaf1aa8e989b50a
William Miller <support@millermobilesoft.com>
2017-06-06 00:00:03 -0400

    Added license and configured for travis.

---
+++ README.md
@@ -0,0 +1,23 @@
+# ViewAnimator
+
+[![CI Status](http://img.shields.io/travis/miller-ms/ViewAnimator.svg?style=flat)](https://travis-ci.org/miller-ms/ViewAnimator)
+[![License](https://img.shields.io/cocoapods/l/MMSProfileImagePicker.svg?style=flat)](http://cocoapods.org/pods/MMSProfileImagePicker)
+[![Platform](https://img.shields.io/cocoapods/p/MMSProfileImagePicker.svg?style=flat)]()
+
+ViewAnimator is a developer utility that allows you to evaluate the different animations and transitions available by the static methods of the UIView class.  
+<p align="center">
+<img src="screenshot.gif" alt="Example">
+</p>
+## Basic Usage
+
+To run the example project, clone the repo, and run `pod install` from the Example directory first.
+
+Import the class header.
+
+## Author
+
+William Miller, support@millermobilesoft.com
+
+## License
+
+This project is is available under the MIT license. See the LICENSE file for more info. Add attibution by linking to the [project page](https://github.com/miller-ms/MMSProfileImagePicker) is appreciated.
--- ViewAnimator/OptionCell.swift
+++ ViewAnimator/OptionCell.swift
@@ -2,8 +2,29 @@
 //  OptionCell.swift
 //  ViewAnimator
 //
-//  Created by William Miller on 5/7/17.
-//  Copyright © 2017 Miller Mobilesoft. All rights reserved.
+//  Created by William Miller DBA Miller Mobilesoft on 5/7/17.
+//
+//  This application is intended to be a developer tool for evaluating the
+//  options for creating an animation or transition using the UIView static
+//  methods UIView.animate and UIView.transition.
+//  Copyright © 2017 William Miller DBA Miller Mobilesoft
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  If you would like to reach the developer you may email me at
+//  support@millermobilesoft.com or visit my website at
+//  http://millermobilesoft.com
 //
 
 import UIKit
